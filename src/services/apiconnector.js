150a3024dd8ee9d15ccf4d77ddeabe789b54b3a0
Ansorn07 <babjirakhi123@gmail.com>
2025-07-25 00:41:39 +0530

    files added

--- src/services/apiconnector.js
+++ src/services/apiconnector.js
@@ -41,30 +41,17 @@
 //   }
 // };
 
-import axios from "axios";
 
-// ✅ Create an Axios instance with cookie support
-export const axiosInstance = axios.create({
-  withCredentials: true, // Necessary for sending/receiving cookies (auth)
-  // baseURL: process.env.REACT_APP_BACKEND_URL, // Optional if you want to set base URL
-});
+import axios from "axios"
 
-// ✅ Reusable API connector function
-export const apiConnector = (
-  method,
-  url,
-  bodyData = {},
-  headers = {},
-  params = {}
-) => {
-  return axiosInstance({
-    method,
-    url,
-    data: Object.keys(bodyData).length ? bodyData : undefined,
-    headers: {
-      "Content-Type": "application/json",
-      ...headers,
-    },
-    params,
-  });
-};
+export const axiosInstance = axios.create({});
+
+export const apiConnector = (method, url, bodyData, headers, params) => {
+    return axiosInstance({
+        method:`${method}`,
+        url:`${url}`,
+        data: bodyData ? bodyData : null,
+        headers: headers ? headers: null,
+        params: params ? params : null,
+    });
+}
